889f19c689e5f7b2caf9c7c373ca50502dc3cb18
ReshmaDas <reshmalakshmi23@gmail.com>
2023-02-07 16:59:56 +0530

    chatapp

---
+++ lib/shared/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:182177381240:android:b04bd7fa7f5ba1f6bee68d";
+  //1:182177381240:web:b78008f4ef54d2d2bee68d
+  static String apiKey = "AIzaSyAVEaP1y3X7uFX7RXKtGZRFFRYYFIesbpM";
+  //AIzaSyAVEaP1y3X7uFX7RXKtGZRFFRYYFIesbpM
+  static String messagingSenderId = "182177381240";
+  //182177381240
+  static String projectId = "chattappflutter-b04cb";
+  //chattappflutter-b04cb
+  final primaryColor = const Color(0xFF0D47A1);
+}
+//AIzaSyBCTslhD1iX7q-7b-wYAAq-V0vfNkTVN9A
